9233f7f5786ae7864a39cc3f0dc64d6b1b0cc3c3
rehan <rehanchalanaprsnl@gmail.com>
2024-06-14 20:35:23 +0530

    feat: created Book DTO's

---
+++ src/main/java/com/rehan/librarymanagementsystem/book/dto/BookRequestDTO.java
@@ -0,0 +1,32 @@
+package com.rehan.librarymanagementsystem.book.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class BookRequestDTO {
+
+     @NotNull
+     @Size(min = 2 , max = 100)
+     private String title;
+
+     @NotNull
+     @Size(min = 13 , max = 14)
+     private String ISBN;
+
+     @NotNull
+     @Past(message = "publicationDate can't be in future")
+     private java.time.LocalDate publicationDate;
+
+     @NotNull
+     @Size(min = 3 , max = 20)
+     private String genre;
+
+     @Min(value = 1)
+     @NotNull
+     private int authorId;
+
+}
