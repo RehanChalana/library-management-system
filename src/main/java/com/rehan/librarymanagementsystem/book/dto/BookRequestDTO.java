a60d3ef00e9737b7455c03fa8b8d002df8041d19
rehan <rehanchalanaprsnl@gmail.com>
2024-06-16 14:13:15 +0530

    refactor: changed book dtos to records

--- src/main/java/com/rehan/librarymanagementsystem/book/dto/BookRequestDTO.java
+++ src/main/java/com/rehan/librarymanagementsystem/book/dto/BookRequestDTO.java
@@ -1,32 +1,10 @@
 package com.rehan.librarymanagementsystem.book.dto;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Past;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
-@Data
-public class BookRequestDTO {
-
-     @NotNull
-     @Size(min = 2 , max = 100)
-     private String title;
-
-     @NotNull
-     @Size(min = 13 , max = 14)
-     private String ISBN;
-
-     @NotNull
-     @Past(message = "publicationDate can't be in future")
-     private java.time.LocalDate publicationDate;
-
-     @NotNull
-     @Size(min = 3 , max = 20)
-     private String genre;
-
-     @Min(value = 1)
-     @NotNull
-     private int authorId;
+public record BookRequestDTO(@NotNull @Size(min = 2, max = 100) String title,
+                             @NotNull @Size(min = 13, max = 14) String ISBN, @NotNull @Past java.time.LocalDate publicationDate,
+                             @NotNull @Size(min = 3, max = 20) String genre,@Positive @NotNull int authorId) {
 
 }
--- src/main/java/com/rehan/librarymanagementsystem/book/dto/BookResponseDTO.java
+++ src/main/java/com/rehan/librarymanagementsystem/book/dto/BookResponseDTO.java
@@ -1,37 +1,11 @@
 package com.rehan.librarymanagementsystem.book.dto;
 
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Past;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
-@Data
-public class BookResponseDTO {
 
-    @NotNull
-    @Min(value = 1)
-    private int bookId;
-
-    @NotNull
-    @Size(min = 2 , max = 100)
-    private String title;
-
-    @NotNull
-    @Size(min = 13 , max = 14)
-    private String ISBN;
-
-    @NotNull
-    @Past(message = "publicationDate can't be in future")
-    private java.time.LocalDate publicationDate;
-
-    @NotNull
-    @Size(min = 3 , max = 20)
-    private String genre;
-
-    @Min(value = 1)
-    @NotNull
-    private int authorId;
+public record BookResponseDTO(@NotNull @Positive int bookId, @NotNull @Size(min = 2, max = 100) String title,
+                              @NotNull @Size(min = 13, max = 14) String ISBN, @Past @NotNull java.time.LocalDate publicationDate,
+                              @NotNull @Size(min = 3, max = 20) String genre, @Positive @NotNull int authorId) {
 
 }
