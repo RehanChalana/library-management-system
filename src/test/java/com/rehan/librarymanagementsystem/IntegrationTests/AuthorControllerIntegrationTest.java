2504ead7cc4b0703eaf769feabf13902f4a6dd1c
rehan <rehanchalanaprsnl@gmail.com>
2024-06-29 20:19:13 +0530

    test: added integration tests for authorController
    findByIdSuccessTest
    findByIdNotFoundTest
    deleteByIdSuccessTest
    deleteByIdNotFoundTest
    findAllTest
    saveNewAuthorTest
    updateAuthorSuccessTest
    updateAuthorNotFoundTest

--- src/test/java/com/rehan/librarymanagementsystem/IntegrationTests/AuthorControllerIntegrationTest.java
+++ src/test/java/com/rehan/librarymanagementsystem/IntegrationTests/AuthorControllerIntegrationTest.java
@@ -1,5 +1,6 @@
 package com.rehan.librarymanagementsystem.IntegrationTests;
 
+import com.rehan.librarymanagementsystem.author.dto.AuthorRequestDTO;
 import com.rehan.librarymanagementsystem.author.dto.AuthorResponseDTO;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -8,13 +9,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.jdbc.Sql;
 import org.testcontainers.containers.PostgreSQLContainer;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD,scripts = {"/schema.sql","/data.sql"})
+@Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD,scripts = "/clear.sql")
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AuthorControllerIntegrationTest {
@@ -32,6 +39,7 @@ public class AuthorControllerIntegrationTest {
         postgres.start();
     }
 
+
     @AfterAll
     static void afterAll() {
         postgres.stop();
@@ -44,12 +52,84 @@ public class AuthorControllerIntegrationTest {
         registry.add("spring.datasource.password",postgres::getPassword);
     }
 
+
+
     @Test
-    public void findByIdTest() {
+    public void findByIdSuccessTest() {
         String url = "http://localhost:" + port + "/api/authors/1";
         ResponseEntity<AuthorResponseDTO> response = restTemplate.getForEntity(url,AuthorResponseDTO.class);
+        AuthorResponseDTO result = response.getBody();
         assertEquals(response.getStatusCode(), HttpStatus.OK);
+        assertNotNull(result);
+        assertEquals(1,result.authorId());
+        assertEquals("George Orwell",result.name());
     }
 
+    @Test
+    public void findByIdNotFoundTest() {
+        String url = "http://localhost:" + port + "/api/authors/10";
+        ResponseEntity<AuthorResponseDTO> response = restTemplate.getForEntity(url,AuthorResponseDTO.class);
+        assertEquals(response.getStatusCode(),HttpStatus.NOT_FOUND);
+    }
+
+    @Test
+    public void deleteByIdSuccessTest() {
+        String url = "http://localhost:" + port + "/api/authors/6";
+        ResponseEntity<?> response =  restTemplate.exchange(url,HttpMethod.DELETE, HttpEntity.EMPTY,Void.class);
+        assertEquals(response.getStatusCode(),HttpStatus.OK);
+    }
+
+    @Test
+    public void findAllTest() {
+        String url = "http://localhost:" + port + "/api/authors";
+        ResponseEntity<List<AuthorResponseDTO>> response = restTemplate.exchange(url, HttpMethod.GET, HttpEntity.EMPTY, new ParameterizedTypeReference<List<AuthorResponseDTO>>() {
+        });
+        List<AuthorResponseDTO> responseDTOS = response.getBody();
+        assertEquals(response.getStatusCode(),HttpStatus.OK);
+        assertNotNull(responseDTOS);
+        assertEquals(6,responseDTOS.size());
+    }
+
+    @Test
+    public void deleteByIdNotFoundTest() {
+        String url = "http://localhost:" + port + "/api/authors/10";
+        ResponseEntity<?> response =  restTemplate.exchange(url,HttpMethod.DELETE, HttpEntity.EMPTY,Void.class);
+        assertEquals(response.getStatusCode(),HttpStatus.NOT_FOUND);
+    }
+
+    @Test
+    public void saveNewAuthorTest() {
+        String url = "http://localhost:" + port + "/api/authors";
+        AuthorRequestDTO request = new AuthorRequestDTO("Marilyn Jain");
+        ResponseEntity<AuthorResponseDTO> response = restTemplate.postForEntity(url,request,AuthorResponseDTO.class);
+        AuthorResponseDTO resultDTO = response.getBody();
+        assertEquals(response.getStatusCode(),HttpStatus.CREATED);
+        assertNotNull(resultDTO);
+        assertEquals(7,resultDTO.authorId());
+        assertEquals("Marilyn Jain",resultDTO.name());
+    }
+
+    @Test
+    public void updateAuthorSuccessTest() {
+        String url = "http://localhost:" + port + "/api/authors/6";
+        AuthorRequestDTO requestDTO = new AuthorRequestDTO("changed Name");
+        HttpEntity<AuthorRequestDTO> request = new HttpEntity<>(requestDTO);
+        ResponseEntity<AuthorResponseDTO> response = restTemplate.exchange(url,HttpMethod.PUT,request,AuthorResponseDTO.class);
+        AuthorResponseDTO responseDTO = response.getBody();
+
+        assertEquals(response.getStatusCode(),HttpStatus.OK);
+        assertNotNull(responseDTO);
+        assertEquals(6,responseDTO.authorId());
+        assertEquals("changed Name",responseDTO.name());
+    }
+
+    @Test
+    public void updateAuthorNotFoundTest() {
+        String url = "http://localhost:" + port + "/api/authors/10";
+        AuthorRequestDTO requestDTO = new AuthorRequestDTO("changed Name");
+        HttpEntity<AuthorRequestDTO> request = new HttpEntity<>(requestDTO);
+        ResponseEntity<AuthorResponseDTO> response = restTemplate.exchange(url,HttpMethod.PUT,request,AuthorResponseDTO.class);
+        assertEquals(response.getStatusCode(),HttpStatus.NOT_FOUND);
+    }
 
 }
